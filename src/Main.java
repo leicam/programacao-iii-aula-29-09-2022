827bc3a5b4b84015abb8e87a8ee2cf7e38ff6335
Juliano Ribeiro de Souza Maciel <juliano.maciel@totvs.com.br>
2022-10-06 14:46:37 -0300

    Implementada pesquisa em largura

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.Map;
+import javax.management.Query;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,8 +27,47 @@ public class Main {
 
         tabela.put("Juliano", vizinhosJuliano);
         tabela.put("Rhuan", vizinhosRhuan);
-        tabela.put("Cauan", vizinhosCaua);
+        tabela.put("Caua", vizinhosCaua);
 
+        showData(tabela);
+
+        System.out.println(pesquisaEmLargura(tabela, "Rhuan"));
+    }
+
+    private static String pesquisaEmLargura(Hashtable<String, ArrayList<String>> tabela, String verticeInicial){
+        Queue<String> fila = new LinkedList<String>();
+        ArrayList<String> verificados = new ArrayList<String>();
+
+        fila = enfileirar(fila, (ArrayList<String>)tabela.get(verticeInicial));
+
+        while(fila.size() > 0){
+            String pessoa = fila.poll();
+
+            if ("Felipe".equals(pessoa))
+                return "Felipe foi encontrado!";
+
+            if (verificados.contains(pessoa))
+                continue;
+
+            fila = enfileirar(fila, (ArrayList<String>)tabela.get(pessoa));
+            verificados.add(pessoa);
+        }
+
+        return "Felipe não foi encontrado!";
+    }
+
+    private static Queue<String> enfileirar(Queue<String> fila, ArrayList<String> lista){
+        if (lista == null)
+            return fila;
+
+        for (String item : lista){
+            fila.offer(item);
+        }
+
+        return fila;
+    }
+
+    private static void showData(Hashtable<String, ArrayList<String>> tabela){
         for (Map.Entry<String, ArrayList<String>> entry : tabela.entrySet()){
             String chave = entry.getKey();
             ArrayList<String> dados = entry.getValue();
